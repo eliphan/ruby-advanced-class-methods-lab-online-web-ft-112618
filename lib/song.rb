855768e34e34c17fc4569f4e24492841f2b24ca1
Elise Phan <aliceconnic@gmail.com>
2018-12-05 21:01:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,40 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+   song = self.new
+   song.save
+   song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end  
+    
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+    
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name)
+     if song.name == 
+       song
+     else
+       self.create_by_name(name)
+      end
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{ |a, b| a <=> b }
+  end
+  
 end
